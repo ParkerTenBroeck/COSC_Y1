76b199073bca7229cdc144aba185e882b8f0b6cf
Parker TenBroeck <51721964+ParkerTenBroeck@users.noreply.github.com>
2022-10-17 16:53:30 -0400

    Finished Assignment 1

--- BlueJ/Assign_1/SnowflakesB.java
+++ BlueJ/Assign_1/SnowflakesB.java
@@ -1,10 +1,7 @@
 import Media.*;
 
 /**
- * Write a description of class SnowflakesB here.
- *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Parker TenBroeck 7376726
  */
 public class SnowflakesB
 {
@@ -19,9 +16,12 @@ public class SnowflakesB
         
         double sideLen = Math.random() * 19.0 + 5.0;
         
+        // draw the 5 sides of the snowflake 
         for(int edge = 0; edge < 5; edge++){
             turtle.forward(sideLen);
             turtle.left(Math.PI * 3.0/5.0);
+            
+            // draws the small unclosed pentagon on each snowflake side
             for(int side = 0; side < 4; side++){
                 turtle.forward(sideLen);            
                 turtle.right(Math.PI * 2.0/5.0);           
--- BlueJ/Assign_1/SnowflakesC.java
+++ BlueJ/Assign_1/SnowflakesC.java
@@ -1,5 +1,8 @@
 import Media.*;
 
+/**
+ * @author Parker TenBroeck 7376726
+ */
 public class SnowflakesC
 {
 
@@ -10,15 +13,18 @@ public class SnowflakesC
         TurtleDisplayer display = new TurtleDisplayer(turtle);
         turtle.setSpeed(0);
         
+        // draws 5 snowflakes at a random position and with a random size
         for(int snowflake = 0; snowflake < 5; snowflake++){
             double sideLen = Math.random() * 19.0 + 5.0;
             double x = Math.random() * 300.0 -150.0;
             double y = Math.random() * 300.0 -150.0;
             turtle.moveTo(x,y);
             turtle.penDown();
+            // draw the 5 sides of the snowflake 
             for(int edge = 0; edge < 5; edge++){
                 turtle.forward(sideLen);
                 turtle.left(Math.PI * 3.0/5.0);
+                // draws the small unclosed pentagon on each snowflake side
                 for(int side = 0; side < 4; side++){
                     turtle.forward(sideLen);            
                     turtle.right(Math.PI * 2.0/5.0);           
