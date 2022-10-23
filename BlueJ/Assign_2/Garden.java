d2b0acb5c0d22e6102086eb8054076933b8eecde
Parker TenBroeck <51721964+ParkerTenBroeck@users.noreply.github.com>
2022-10-23 15:20:44 -0400

    Assignment 2

--- BlueJ/Assign_2/Garden.java
+++ BlueJ/Assign_2/Garden.java
@@ -15,7 +15,11 @@ public class Garden
         drawGarden();
     }
     
+    /**
+     * Location and angle of this.turtle is preserved with this procedure
+     */
     public void drawPetal() {
+        // drawing the pedal acording to the lesson PDF
         this.turtle.penDown();
         this.turtle.left(Math.PI);
         this.turtle.left(Math.PI / 4.0);
@@ -31,8 +35,10 @@ public class Garden
     }
     
     public void drawFlower() {
+        // random number of petals between 4 and 7
         int petalNumber = (int)(Math.random() * 4) + 4;
         
+        // the angle to rotate each consecutive pedal by
         double angle = Math.PI * 2.0 / petalNumber;
         for (int i = 0; i < petalNumber; i ++){
             this.drawPetal();
@@ -41,16 +47,22 @@ public class Garden
     }
     
     public void drawGarden(){
-
+        // height and width of screen
         double height = 300;
         double width = 300;
+        // radius of the flowers (center to tip of pedal)
         double rad = Math.sin(Math.PI / 3.0) * 30 + 15;
         
+        // loops through each flower (16 in total)
         for (int x = 0; x < 4; x ++){
             for (int y = 0; y < 4; y ++){
-                double mx = x * ((width - rad * 2) / 3.0) + rad - width / 2.0;
-                double my = y * ((height - rad * 2) / 3.0) + rad - height / 2.0;
-                this.turtle.moveTo(mx, my);
+                // calculate the center x and y position of each flower
+                // so that flowers are evenly spaced and the flowers are always
+                // visible on screen
+                double fx = x * ((width - rad * 2) / 3.0) + rad - width / 2.0;
+                double fy = y * ((height - rad * 2) / 3.0) + rad - height / 2.0;
+                this.turtle.penUp();
+                this.turtle.moveTo(fx, fy);
                 this.drawFlower();
             }
         }
